819f4f44e9f4863d0a75b909e5f35b2b1d18be7d
Poya Faraji <poya.faraji@outlook.com>
2024-07-26 01:31:39 +0330

    Handle Pagination
    Description:
         - Handled search and pagination with URL search parameters instead of client state.
         - Fetched data on the server.
         - Using the useRouter router hook for smoother, client-side transitions.

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -5,6 +5,7 @@ import { CreateInvoice } from "@/app/ui/invoices/buttons";
 import { lusitana } from "@/app/ui/fonts";
 import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
 import { Suspense } from "react";
+import { fetchInvoicesPages } from "@/app/lib/data";
 
 interface InvoiceProps {
   searchParams?: {
@@ -14,10 +15,9 @@ interface InvoiceProps {
 }
 
 export default async function Page({ searchParams }: InvoiceProps) {
-
-  const query = searchParams?.query || '';
+  const query = searchParams?.query || "";
   const currentPage = Number(searchParams?.page) || 1;
-
+  const totalPages  = await fetchInvoicesPages(query);
 
   return (
     <div className="w-full">
@@ -32,7 +32,7 @@ export default async function Page({ searchParams }: InvoiceProps) {
         <Table query={query} currentPage={currentPage} />
       </Suspense>
       <div className="mt-5 flex w-full justify-center">
-        {/* <Pagination totalPages={totalPages} /> */}
+        <Pagination totalPages={totalPages} />
       </div>
     </div>
   );
